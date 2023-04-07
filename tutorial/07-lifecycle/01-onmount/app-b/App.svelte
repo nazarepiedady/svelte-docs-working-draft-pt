6d8927676a0e71b9939d547a6a44ea9c97c2829d
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-04-07 06:42:59 +0100

    docs(pt): translate `tutorial/lifecycle/onmount/app-b/app.svelte`

--- tutorial/07-lifecycle/01-onmount/app-b/App.svelte
+++ tutorial/07-lifecycle/01-onmount/app-b/App.svelte
@@ -18,7 +18,7 @@
 			<figcaption>{photo.title}</figcaption>
 		</figure>
 	{:else}
-		<!-- this block renders when photos.length === 0 -->
+		<!-- Este bloco apresenta quando photos.length === 0 -->
 		<p>loading...</p>
 	{/each}
 </div>
